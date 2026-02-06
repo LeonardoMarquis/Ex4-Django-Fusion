1c6f27e21ee94afab5bcfd848290b076980ce81e
LeonardoMarquis <marquisleonardo12@gmail.com>
2026-02-06 06:57:00 -0300

    comeco assunto testes

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.views.generic import TemplateView, FormView
 
 from .models import Service, Employee
+from .forms import ContactForm
 
-class IndexView(TemplateView):
+
+'''class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
@@ -10,7 +14,34 @@ class IndexView(TemplateView):
         context['services'] = Service.objects.all()
         context['employees'] = Employee.objects.all()
 
+        return context'''
+
+class IndexView(FormView):
+    template_name = "index.html"
+    form_class = ContactForm
+    success_url = reverse_lazy('index')
+
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Service.objects.all()
+        context['employees'] = Employee.objects.all()
+
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_email()
+        messages.success(self.request, 'Message sent successfully!')
+        
+        return super().form_valid(form, *args, **kwargs)
+    
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Error sending message. Please try again.')
+        
+        return super().form_invalid(form, *args, **kwargs)
+
+
+
 
 class TesteView(TemplateView):
     template_name = "teste.html"
--- fusion/settings.py
+++ fusion/settings.py
@@ -121,3 +121,18 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# email para ativar quando estiver no ar
+
+'''
+EMAIL_HOST = localhost
+EMAIL_HOST_USER = no-reply@seu_dominio.com
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = fusion
+DEFAULT_FROM_EMAIL = contato@fusion.com.br
+'''
